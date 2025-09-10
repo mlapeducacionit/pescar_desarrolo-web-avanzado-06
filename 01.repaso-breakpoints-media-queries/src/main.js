45681820d687da3cd7fde62ff6dfc61545514143
Maximiliano Principe <mlapeducacionit@gmail.com>
2025-09-10 16:50:19 -0300

    trabajamos con el menú hamburguesa en js

--- 01.repaso-breakpoints-media-queries/src/main.js
+++ 01.repaso-breakpoints-media-queries/src/main.js
@@ -1,2 +1,12 @@
 import './style.css'
 
+const hamburguesa = document.querySelector('.hamburguesa')
+console.log(hamburguesa) // Objeto de JS que representa un elemento html
+console.dir(hamburguesa) // Le sadco una radiografía al nodo. 
+console.log(hamburguesa.innerText) // ☰
+console.log(hamburguesa.textContent) // ☰
+const nav = document.querySelector('nav')
+
+hamburguesa.addEventListener('click', () => {
+    nav.classList.toggle('show')
+})
